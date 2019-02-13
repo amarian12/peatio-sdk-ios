c542ba26d23b2ef6b98361a1e9e457b0e376a09c
wsof401 <oct.song.wu@gmail.com>
2019-02-13 11:22:46 +0800

    address's memo nullable

--- PeatioSDK/API/Model/Address.swift
+++ PeatioSDK/API/Model/Address.swift
@@ -4,6 +4,6 @@ public struct Address: Codable {
     public let chain: String
     public let id: Int
     public let insertedAt: Date
-    public let memo: String
+    public let memo: String?
     public let value: String
 }
